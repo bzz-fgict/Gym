a5b3c78184308f860dba1eb2f9382a501ebab242
Z-Ramirez <96289837+Z-Ramirez@users.noreply.github.com>
2023-07-14 08:02:51 +0200

    Set Training frontend

--- src/main/java/dao/TrainingSetDao.java
+++ src/main/java/dao/TrainingSetDao.java
@@ -1,14 +1,33 @@
 package dao;
 
-import dto.TrainingSet;
+import dto
 
 public class TrainingSetDAO {
+
     public TrainingSet getById(int trainingSetId) {
         // Implementation to retrieve TrainingSet by trainingSetId from the database
+
+
     }
 
     public void save(TrainingSet trainingSet) {
-        // Implementation to save a TrainingSet object to the database
+        String sql = "INSERT INTO trainingset (trainingSetId, exercise, muscleGroup, setAmount, repAmount, details) VALUES (:trainingSetId, :exercise, :muscleGroup, :setAmount, :repAmount, :details)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                .addValue("trainingSetId", training.
+                .addValue("firstname", player.getFirstname())
+                .addValue("secondname", player.getSecondname())
+                .addValue("age", player.getAge().toString())
+                .addValue("teamId", player.getTeamId());
+        int rowsAffected = namedParameterJdbcTemplate.update(sql, namedParameters, keyHolder);
+        if(rowsAffected == 1){
+            System.out.println("Player inserted");
+        }
+        else{
+            System.out.println("Player not inserted");
+        }
+    }
     }
 
     public void update(TrainingSet trainingSet) {
